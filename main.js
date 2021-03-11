acdfefac465db3edbf066176afed2790a1c64641
Lorenzo Bernini <templarec@gmail.com>
2021-03-11 21:11:13 +0100

    Esercizio completato con Bonus

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //cliccando le frecce cambia immagine
 //frecce sono : fa-angle-left, fa-angle-right
 //cambiano i cerchietti
-//le immagini vanno a rotazi
+//le immagini vanno a rotazione
 //creo evento click sulle frecce
 //dichiaro variabili e ci metto le classi delle frecce
 var nextArrow = $(".fa-angle-right");
