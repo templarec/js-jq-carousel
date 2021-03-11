4262c7edb87c17f94b26802e69ee23ac3d34ae41
Lorenzo Bernini <templarec@gmail.com>
2021-03-11 21:09:01 +0100

    Esercizio completato con Bonus

--- main.js
+++ main.js
@@ -74,3 +74,16 @@ $(document).keydown(function (evento) {
 		}
 	}
 });
+
+//bonus
+var nav = $(".nav i");
+
+nav.click(function (){
+	currentImage.removeClass("active"); //tolgo la classe active a tutti gli <img>
+	currentCircle.removeClass("active"); //tolgo la classe active a tutti gli <i>
+	//prendo l'iesimo elemento immagine in base all'iesimo elemento icona pallino cliccato(this)
+	//e assegno active a quell'img e icona pallino
+	currentImage.eq(nav.index(this)).addClass("active");
+	currentCircle.eq(nav.index(this)).addClass("active");
+
+});
